2ec8c959fe5619482a321a74e60563eba121ed8f
KDJDEV <kyledagman7@gmail.com>
2025-12-02 14:08:23 -0500

    Cleaned up edge case logic

--- hopperDisplay.py
+++ hopperDisplay.py
@@ -7,76 +7,47 @@ def knight_calc(R, C, Gr, Gc, Lr, Lc):
     i = abs(Lr - Gr)
     j = abs(Lc - Gc)
 
-    if (Lr > R or Lc > C or Lr < 1 or Lc < 1):
+    # Out of bounds
+    if not (1 <= Lr <= R and 1 <= Lc <= C):
         return "impossible"
+
+    # Single row/column is impossible
     if (R == 1 or C == 1) and (Gr, Gc) != (Lr, Lc):
         return "impossible"
-    if (C == 2):
-        if (not (i % 4 == 0 and j == 0) and not (i % 4 == 2 and j == 1)):
-            return "impossible"
-    if (R == 2):
-        if (not (j % 4 == 0 and i == 0) and not (j % 4 == 2 and i == 1)):
-            return "impossible"
-    if (R == 3 and C == 3 and Gr == 2 and Gc == 2 and (Lr, Lc) != (2, 2)):
+
+    # 2-wide impossible cases
+    if C == 2 and not ((i % 4 == 0 and j == 0) or (i % 4 == 2 and j == 1)):
         return "impossible"
-    if (R == 3 and C == 3 and (Gr, Gc) != (2, 2) and (Lr, Lc) == (2, 2)):
+    if R == 2 and not ((j % 4 == 0 and i == 0) or (j % 4 == 2 and i == 1)):
         return "impossible"
 
-    #corner diagonal edge cases
-    if (
-        i == 1 and
-        j == 1 and
-        (Gr in (1, R) and Gc in (1, C)) and
-        (Lr in (2, R-1) and Lc in (2, C-1))
-    ):
+    # 3x3 center is impossible
+    if R == 3 and C == 3 and (Gr == 2 and Gc == 2) != (Lr == 2 and Lc == 2):
+        return "impossible"
+
+    # Corner diagonal edge cases
+    if i == 1 and j == 1:
+        if (Gr in (1, R) and Gc in (1, C)) and (Lr in (2, R-1) and Lc in (2, C-1)):
+            return 4
+        if (Gr in (2, R-1) and Gc in (2, C-1)) and (Lr in (1, R) and Lc in (1, C)):
+            return 4
+
+    # 4xN and Nx4 corner to corner edge cases
+    if (Gr in (1, R) and Gc in (1, C)) and (Lr in (1, R) and Lc in (1, C)):
+        if i == 3 and j == 0 and R == 4:
+            return 5
+        if i == 0 and j == 3 and C == 4:
+            return 5
+
+    # 3xN and Nx3 2 from the start edge cases
+    if i == 0 and j == 2 and R == 3 and Gr == 2:
         return 4
-    if (
-        i == 1 and
-        j == 1 and
-        (Gr in (2, R-1) and Gc in (2, C-1)) and
-        (Lr in (1, R) and Lc in (1, C))
-    ):
+    if i == 2 and j == 0 and C == 3 and Gc == 2:
         return 4
 
-    #4xN and Nx4 corner to corner edge cases:
-    if (
-        i == 3 and
-        j == 0 and
-        R == 4 and
-        (Gr in (1, R) and Gc in (1, C)) and
-        (Lr in (1, R) and Lc in (1, C))
-    ):
-        return 5
-    
-    if (
-        i == 0 and
-        j == 3 and
-        C == 4 and
-        (Gr in (1, R) and Gc in (1, C)) and
-        (Lr in (1, R) and Lc in (1, C))
-    ):
-        return 5
-
-    #3xN and Nx3 2 from the start edge cases:
-    if (
-        i == 0 and
-        j == 2 and
-        R == 3 and
-        Gr == 2
-    ):
-        return 4
-    if (
-        i == 2 and
-        j == 0 and
-        C == 3 and
-        Gc == 2
-    ):
-        return 4
-    #3x4 and 4x3 center start case
-    if (
-        ((R, C) == (3, 4) and {(Gr, Gc), (Lr, Lc)} == {(2, 2), (2, 3)}) or
-        ((R, C) == (4, 3) and {(Gr, Gc), (Lr, Lc)} == {(2, 2), (3, 2)})
-    ): 
+    # 3x4 and 4x3 center start case
+    if ((R, C) == (3, 4) and {(Gr, Gc), (Lr, Lc)} == {(2, 2), (2, 3)}) or \
+    ((R, C) == (4, 3) and {(Gr, Gc), (Lr, Lc)} == {(2, 2), (3, 2)}):
         return 5
 
     if (i + j == 1):
